1b00a389fe14411bf4a05bb7928bca6916b72fef
hancia <hanna.klimczak@gmail.com>
2018-01-14 00:46:37 +0100

    jakies funkcje

--- braian.py
+++ braian.py
@@ -3,6 +3,7 @@ import requests
 class Braian(object):
 
     def __init__(self):
+        self.contactnumber=690890608
         self.BASE_PATH = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/da417be9-5a0f-4e02-972a-d16c59ee77f8?subscription-key=855fe00606ef48ecb4dafc6a30b92845&verbose=true&timezoneOffset=0&q="
 
     def _replace(self,a):
@@ -16,6 +17,20 @@ class Braian(object):
 
     def _serve_message(self, answer):
         intent = self._best_intent(answer)
+        if intent == "Signal":
+            self._signal_message(answer)
+        elif intent == "None":
+            self._none_message(answer)
+        elif intent == "Calendar.Add":
+            self._calendaradd(answer)
+        else:
+            print("No idea :/")
+
+    def _none_message(self, answer):
+        printf("I don't understand, please try again")
+
+    def _signal_message(self, answer):
+        print("Sending SOS message to the number: ",self.contactnumber)
 
     def run(self):
         while(1):
@@ -23,5 +38,3 @@ class Braian(object):
             answer = self._get_request(input_message)
             self._serve_message(answer)
             print(self._best_intent(answer))
-
-
